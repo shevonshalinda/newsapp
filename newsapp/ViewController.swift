1df4171cd0534353fb46f7160fca432bba639f3d
Digital-04 <digital-04@Digital-04s-iMac.local>
2019-08-16 18:01:42 +0530

    Initial Commit

---
+++ newsapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsapp
+//
+//  Created by Digital-04 on 8/16/19.
+//  Copyright © 2019 NIBM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
